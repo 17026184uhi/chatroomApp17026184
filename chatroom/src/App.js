f2e9cae525a096b22989978becb68ac2baec3f7f
Richard Bradley <17026184uhi@gmail.com>
2024-11-13 09:29:37 +0000

    Updated Login.js & Chatroom.js

--- chatroom/src/App.js
+++ chatroom/src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login";
+import Chatroom from "./components/Chatroom";
 
 function App() {
   return (
     <div>
       <Login />
+      <Chatroom />
     </div>
   );
 }
