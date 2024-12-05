c868b5eae09351b02c016bb26681a3056c5141fc
Richard Bradley <17026184uhi@gmail.com>
2024-12-05 10:12:06 +0000

    added imports and used useState and useEffect and also created function getChats in Chatroom.js  the function getChats gets all the chats documents as a collection from the firestore. added import of db firebase and import of  collection, getDocs all to Chatroom.js

--- chatroom/src/components/Chatroom.js
+++ chatroom/src/components/Chatroom.js
@@ -1,4 +1,18 @@
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+async function getChats() {
+  const snapshot = await getDocs(collection(db, "Chats"));
+  snapshot.forEach((doc) => {
+    console.log(doc.id, "=>", doc.data());
+  });
+}
+
 function Chatroom() {
+  useEffect(() => {
+    getChats();
+  }, []);
   return (
     <div>
       <h1>Chatroom</h1>
