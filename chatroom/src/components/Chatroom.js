5286fbd0fba826e13057396d09733444abd23820
Richard Bradley <17026184uhi@gmail.com>
2024-12-05 12:51:22 +0000

    Updated Chatroom.js to get the messages from the database as well as capture new ones and write them

--- chatroom/src/components/Chatroom.js
+++ chatroom/src/components/Chatroom.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { db } from "../firebase";
-import { collection, onSnapshot, query } from "firebase/firestore";
+import { db, auth } from "../firebase";
+import { collection, onSnapshot, query, addDoc } from "firebase/firestore";
 
 function Chatroom() {
+  const [text, setText] = useState("");
+  const [userId, setUserId] = useState("");
+  const [localMessages, setLocalMessages] = useState([]);
+
   useEffect(() => {
+    setUserId(auth?.currentUser?.uid);
     const q = query(collection(db, "Chats"));
     const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let messages = [];
       QuerySnapshot.forEach((doc) => {
         console.log(doc.id, "=>", doc.data());
+        messages.push(doc.data());
       });
+      setLocalMessages(messages);
     });
 
     return () => {
@@ -33,26 +41,28 @@ function Chatroom() {
             marginLeft: 24,
             marginright: 24,
             overflow: "auto",
-            marginbottom: 24
+            marginBottom: 24
           }}
         >
-          <div
-            style={{ display: "flex", flex: 1, justifyContent: "flex-start" }}
-          >
+          {localMessages.map((localMessage) => (
             <div
-              style={{
-                minHeight: 52,
-                width: 600,
-                backgroundColor: "red",
-                marginTop: 24,
-                paddingLeft: 24,
-                paddingRight: 24,
-                borderRadius: 12
-              }}
+              style={{ display: "flex", flex: 1, justifyContent: "flex-start" }}
             >
-              <p>Message from someone else</p>
+              <div
+                style={{
+                  minHeight: 52,
+                  width: 600,
+                  backgroundColor: "red",
+                  marginTop: 24,
+                  paddingLeft: 24,
+                  paddingRight: 24,
+                  borderRadius: 12
+                }}
+              >
+                <p>{localMessage.content}</p>
+              </div>
             </div>
-          </div>
+          ))}
           <div style={{ display: "flex", flex: 1, justifyContent: "flex-end" }}>
             <div
               style={{
@@ -68,19 +78,41 @@ function Chatroom() {
               <p>Message from me</p>
             </div>
           </div>
-          <input style={{ flex: 11, hight: 32, fontSize: 28 }} />
-          <button
-            style={{
-              flex: 1,
-              backgroundColor: "blue",
-              color: "white",
-              fontWeight: "bold",
-              fontSize: 18,
-              borderWidth: 0
-            }}
+          <div
+            style={{ display: "flex", flexDirections: "row", marginTop: 24 }}
           >
-            Send
-          </button>
+            <input
+              style={{ flex: 11, height: 32, fontSize: 28 }}
+              type="text"
+              value={text}
+              onChange={(value) => {
+                setText(value.target.value);
+              }}
+            />
+            <button
+              style={{
+                flex: 1,
+                backgroundColor: "blue",
+                color: "white",
+                fontWeight: "bold",
+                fontSize: 18,
+                borderWidth: 0
+              }}
+              onClick={async () => {
+                const timestamp = Date.now();
+                const content = text;
+                const uid = userId;
+                const message = { content, timestamp, uid };
+                const docRef = await addDoc(collection(db, "Chats"), message);
+                console.log("Document written with ID: ", docRef.id);
+                if (docRef?.id) {
+                  setText("");
+                }
+              }}
+            >
+              Send
+            </button>
+          </div>
         </div>
       </div>
     </div>
