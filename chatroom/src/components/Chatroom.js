c0bd0cfb88fd30f12760fb857701c762c438935f
Richard Bradley <17026184uhi@gmail.com>
2024-12-05 15:43:43 +0000

    Debugged changes so the messages now appear in order and with non-empty text entries

--- chatroom/src/components/Chatroom.js
+++ chatroom/src/components/Chatroom.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { db, auth } from "../firebase.js";
-import { collection, onSnapshot, query, addDoc } from "firebase/firestore";
+import { db, auth } from "../firebase";
+import {
+  collection,
+  onSnapshot,
+  query,
+  addDoc,
+  orderBy
+} from "firebase/firestore";
 
 function Chatroom() {
   const [text, setText] = useState("");
@@ -8,13 +14,13 @@ function Chatroom() {
   const [localMessages, setLocalMessages] = useState([]);
 
   useEffect(() => {
-    setUserId(auth.currentUser?.uid);
+    setUserId(auth?.currentUser?.uid);
 
-    const q = query(collection(db, "Chats"));
+    const q = query(collection(db, "Chats"), orderBy("timestamp", "asc"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let messages = [];
       querySnapshot.forEach((doc) => {
-        console.log(doc.id, "", doc.data());
+        console.log(doc.id, "=>", doc.data());
         messages.push(doc.data());
       });
       setLocalMessages(messages);
@@ -50,14 +56,14 @@ function Chatroom() {
                 display: "flex",
                 flex: 1,
                 justifyContent:
-                  userId == localMessage.uid ? "flex-end" : "flex-start"
+                  userId === localMessage.uid ? "flex-end" : "flex-start"
               }}
             >
               <div
                 style={{
                   minHeight: 52,
                   width: 600,
-                  backgroundColor: userId == localMessage.uid ? "blue" : "red",
+                  backgroundColor: userId === localMessage.uid ? "blue" : "red",
                   marginTop: 24,
                   paddingLeft: 24,
                   paddingRight: 24,
@@ -74,16 +80,17 @@ function Chatroom() {
             style={{
               flex: 11,
               height: 32,
-              fontSize: 28,
-              type: "text",
-              value: text,
-              onChange: (value) => {
-                setText(value.target.value);
-              }
+              fontSize: 28
+            }}
+            type="text"
+            value={text}
+            onChange={(value) => {
+              setText(value.target.value);
             }}
           />
           <button
             style={{
+              flex: 1,
               backgroundColor: "blue",
               color: "white",
               fontWeight: "bold",
@@ -97,7 +104,9 @@ function Chatroom() {
               const message = { content, timestamp, uid };
               const docRef = await addDoc(collection(db, "Chats"), message);
               console.log("Document written with ID: ", docRef.id);
-              setText("");
+              if (docRef?.id) {
+                setText("");
+              }
             }}
           >
             Send
@@ -109,114 +118,3 @@ function Chatroom() {
 }
 
 export default Chatroom;
-
-// import React, { useState, useEffect } from "react";
-// import { db, auth } from "../firebase";
-// import { collection, onSnapshot, query, addDoc } from "firebase/firestore";
-
-// function Chatroom() {
-//   const [text, setText] = useState("");
-//   const [userId, setUserId] = useState("");
-//   const [localMessages, setLocalMessages] = useState([]);
-
-//   useEffect(() => {
-//     setUserId(auth?.currentUser?.uid);
-//     const q = query(collection(db, "Chats"));
-//     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-//       let messages = [];
-//       querySnapshot.forEach((doc) => {
-//         console.log(doc.id, "=>", doc.data());
-//         messages.push(doc.data());
-//       });
-//       setLocalMessages(messages);
-//     });
-
-//     return () => {
-//       unsubscribe();
-//     };
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Chatroom</h1>
-//       <div
-//         style={{
-//           display: "flex",
-//           flex: 1,
-//           height: "100vh",
-//           flexdirection: "column"
-//         }}
-//       >
-//         <div
-//           style={{
-//             flex: 1,
-//             marginLeft: 24,
-//             marginRight: 24,
-//             overflow: "auto",
-//             marginBottom: 24
-//           }}
-//         >
-//           {localMessages.map((localMessage) => (
-//             <div
-//               style={{
-//                 display: "flex",
-//                 flex: 1,
-//                 justifyContent:
-//                   userId === localMessage.uid ? "flex-end" : "flex-start"
-//               }}
-//             >
-//               <div
-//                 style={{
-//                   minHeight: 52,
-//                   width: 600,
-//                   backgroundColor: userId === localMessage.uid ? "blue" : "red",
-//                   marginTop: 24,
-//                   paddingLeft: 24,
-//                   paddingRight: 24,
-//                   borderRadius: 12
-//                 }}
-//               >
-//                 <p>{localMessage.content}</p>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//         <div style={{ display: "flex", flexDirections: "row", marginTop: 24 }}>
-//           <input
-//             style={{ flex: 11, height: 32, fontSize: 28 }}
-//             type="text"
-//             value={text}
-//             onChange={(value) => {
-//               setText(value.target.value);
-//             }}
-//           />
-//           <button
-//             style={{
-//               flex: 1,
-//               backgroundColor: "blue",
-//               color: "white",
-//               fontWeight: "bold",
-//               fontSize: 18,
-//               borderWidth: 0
-//             }}
-//             onClick={async () => {
-//               const timestamp = Date.now();
-//               const content = text;
-//               const uid = userId;
-//               const message = { content, timestamp, uid };
-//               const docRef = await addDoc(collection(db, "Chats"), message);
-//               console.log("Document written with ID: ", docRef.id);
-//               if (docRef?.id) {
-//                 setText("");
-//               }
-//             }}
-//           >
-//             Send
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Chatroom;
