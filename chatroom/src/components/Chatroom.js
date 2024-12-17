b939369ca568c005f895577980596c4b0a678b4e
Richard Bradley <17026184uhi@gmail.com>
2024-12-17 17:45:23 +0100

    Corrected case error in line 120 affecting the fireStore db collection of Chats that msgs with images were pushed to

--- chatroom/src/components/Chatroom.js
+++ chatroom/src/components/Chatroom.js
@@ -7,11 +7,15 @@ import {
   addDoc,
   orderBy
 } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 function Chatroom() {
   const [text, setText] = useState("");
   const [userId, setUserId] = useState("");
   const [localMessages, setLocalMessages] = useState([]);
+  const [localImage, setLocalImage] = useState(null);
+
+  const storage = getStorage();
 
   useEffect(() => {
     setUserId(auth?.currentUser?.uid);
@@ -71,46 +75,98 @@ function Chatroom() {
                 }}
               >
                 <p>{localMessage.content}</p>
+                {localMessage?.image && localMessage.image.length > 0 && (
+                  <img
+                    style={{ width: "100%", height: "auto", marginBottom: 24 }}
+                    src={localMessage.image}
+                  />
+                )}
               </div>
             </div>
           ))}
         </div>
         <div style={{ display: "flex", flexDirection: "row", marginTop: 24 }}>
-          <input
-            style={{
-              flex: 11,
-              height: 32,
-              fontSize: 28
-            }}
-            type="text"
-            value={text}
-            onChange={(value) => {
-              setText(value.target.value);
-            }}
-          />
-          <button
-            style={{
-              flex: 1,
-              backgroundColor: "blue",
-              color: "white",
-              fontWeight: "bold",
-              fontSize: 18,
-              borderWidth: 0
-            }}
-            onClick={async () => {
+          <form
+            style={{ display: "flex", flexDirection: "row", flex: 1 }}
+            onSubmit={async (e) => {
+              e.preventDefault(); //prevents reload of the page
               const timestamp = Date.now();
+              let image = "";
               const content = text;
               const uid = userId;
-              const message = { content, timestamp, uid };
-              const docRef = await addDoc(collection(db, "Chats"), message);
-              console.log("Document written with ID: ", docRef.id);
-              if (docRef?.id) {
-                setText("");
+              console.log("User ID = ", uid);
+              if (localImage) {
+                console.log("Local image");
+                const uniquelocalImage = `${
+                  localImage.name
+                }_${Math.random().toString(36)}`;
+                const storageRef = ref(storage, `/images/${uniquelocalImage}`);
+                await uploadBytes(storageRef, localImage).then(
+                  async (snapshot) => {
+                    console.log("uploading bytes...");
+                    const firebaseUrl = await getDownloadURL(storageRef);
+                    console.log("firebaseUrl", firebaseUrl);
+                    const message = {
+                      content,
+                      timestamp,
+                      uid,
+                      image: firebaseUrl
+                    };
+                    console.log(
+                      "about to add the message doc with local image to db"
+                    );
+                    console.log(message);
+                    const docRef = await addDoc(
+                      collection(db, "Chats"),
+                      message
+                    );
+                    console.log("created docRef");
+                  }
+                );
+              } else {
+                console.log("it's a message without an image");
+                const message = { content, timestamp, uid, image };
+                const docRef = await addDoc(collection(db, "Chats"), message);
               }
+              setText("");
+              setLocalImage(null);
             }}
           >
-            Send
-          </button>
+            <input
+              style={{
+                flex: 11,
+                height: 32,
+                fontSize: 28
+              }}
+              type="text"
+              value={text}
+              onChange={(value) => {
+                setText(value.target.value);
+              }}
+            />
+            <input
+              key={Date.now()}
+              style={{ flex: 1 }}
+              type="file"
+              onChange={(e) => {
+                const image = e.target.files[0];
+                setLocalImage(image);
+              }}
+            />
+            <button
+              type="submit"
+              style={{
+                flex: 1,
+                backgroundColor: "blue",
+                color: "white",
+                fontWeight: "bold",
+                fontSize: 18,
+                borderWidth: 0
+              }}
+            >
+              Send
+            </button>
+          </form>
         </div>
       </div>
     </div>
